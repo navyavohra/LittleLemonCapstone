014d518269b3435251ebba140bd94c94005b176e
Navya <vohrana@sheridancollege.ca>
2024-08-26 16:41:50 +0530

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Navya Vohra on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
