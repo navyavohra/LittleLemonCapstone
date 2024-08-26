addfa4df007eb0bc1e80483fd685cbf452c6ab9a
Navya <vohrana@sheridancollege.ca>
2024-08-26 17:21:51 +0530

    Added Menu Category in Model

---
+++ LittleLemon/Model/MenuCategory.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCategory.swift
+//  LittleLemon
+//
+//  Created by Navya Vohra on 2024-08-26.
+//
+
+import Foundation
+
+struct MenuCategory{
+    let name: String
+    let items: [MenuItem]
+}
